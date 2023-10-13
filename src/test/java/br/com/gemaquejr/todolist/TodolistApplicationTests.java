ad4cf90879512ece66e48570230466e15f934466
gemaquejr <gemaquejr@hotmail.com>
2023-10-13 20:10:46 -0300

    Adiciona estrutura do projeto
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

---
+++ src/test/java/br/com/gemaquejr/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gemaquejr.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
