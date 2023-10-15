8920f1929298bdf3c4fdaac42a5c306ca78f05dd
gemaquejr <gemaquejr@hotmail.com>
2023-10-14 22:49:47 -0300

    Adiciona método de atualização específica de uma tarefa
    Signed-off-by: gemaquejr <gemaquejr@hotmail.com>

--- src/main/java/br/com/gemaquejr/todolist/task/TaskController.java
+++ src/main/java/br/com/gemaquejr/todolist/task/TaskController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.gemaquejr.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
@@ -53,10 +54,12 @@ public class TaskController {
 
     @PutMapping("/{id}")
     public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
-        var idUser = request.getAttribute("idUser");
-        taskModel.setIdUser((UUID) idUser);
-        taskModel.setId(id);
-        return this.taskRepository.save(taskModel);
+
+        var task = this.taskRepository.findById(id).orElse(null);
+        
+        Utils.copyNonNullProperties(taskModel, task);
+
+        return this.taskRepository.save(task);
     }
     
 }
